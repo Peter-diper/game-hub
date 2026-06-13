5dade5ec99e0d779c20f3daf95cece37a5f36aac
Peter <161234440+Peter-diper@users.noreply.github.com>
2026-06-13 19:05:08 +0330

    implementing simplify qeury object

---
+++ src/hooks/useGenre.ts
@@ -0,0 +1,7 @@
+import useGenres from "./useGenres";
+
+export function useGenre(id?: number) {
+  const { data: genres } = useGenres();
+  const genre = genres.results.find((g) => g.id === id);
+  return genre;
+}
