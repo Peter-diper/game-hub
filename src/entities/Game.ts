1a7d16d7663f4f114bde7caf35daf32aa3bcb3f6
Peter <161234440+Peter-diper@users.noreply.github.com>
2026-06-16 20:06:11 +0330

    build game atributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,9 +1,13 @@
+import { Genre } from "./Genre";
 import { Platform } from "./Platform";
+import { Publisher } from "./Publisher";
 
 export interface Game {
   id: number;
   slug: string;
+  publishers: Publisher[];
   name: string;
+  genres: Genre[];
   background_image: string;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,7 +1,10 @@
-import { Box, Button, Heading, Spinner, Text } from "@chakra-ui/react";
+import { Box, Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
-import useGame from "../hooks/useGame";
 import ExpandableText from "../components/ExpandableText";
+import useGame from "../hooks/useGame";
+import DefinitionItem from "../components/DefinitionItem";
+import CriticScore from "../components/CriticScore";
+import GameAtributes from "../components/GameAtributes";
 export default function GameDetailPage() {
   const { slug } = useParams();
   const { data: game, error, isLoading } = useGame(slug!);
@@ -12,6 +15,7 @@ export default function GameDetailPage() {
     <Box padding={5}>
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameAtributes game={game} />
     </Box>
   );
 }
